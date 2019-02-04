32c584ba2f3c6a4b3d5a5c379d0408488ca87b17
seunkim <seunkim@ea.com>
2019-02-04 21:57:28 +0900

    Added files

---
+++ grafana/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+url="http://$GF_SECURITY_ADMIN_USER:$GF_SECURITY_ADMIN_PASSWORD@localhost:3000"
+
+post() {
+    curl -s -X POST -d "$1" \
+        -H 'Content-Type: application/json;charset=UTF-8' \
+        "$url$2" 2> /dev/null
+}
+
+if [ ! -f "/var/lib/grafana/.init" ]; then
+
+    echo "Start initial.. "
+    exec /run.sh $@ &
+
+    until curl -s "$url/api/datasources" 2> /dev/null; do
+        sleep 1
+    done
+
+    for datasource in /etc/grafana/datasources/*; do
+        post "$(envsubst < $datasource)" "/api/datasources"
+    done
+
+    sed -ie '1s/^/\{\"dashboard\"\:/' /app/gatling.json
+    echo '}' >> /app/gatling.json
+    post "@/app/gatling.json" "/api/dashboards/db"
+
+    touch "/var/lib/grafana/.init"
+
+    kill $(ps aux | grep grafana | awk '{print $2}')
+
+fi
+
+exec /run.sh $@
